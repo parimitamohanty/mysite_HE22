fc4c89d93d841aba039ebbc11323b7c6de17a65a
Parimita Mohanty <pmohanty@unomaha.edu>
2018-09-26 23:45:52 -0500

    updated code

--- blog/models.py
+++ blog/models.py
@@ -1,40 +1,44 @@
 from django.db import models
-
-
-from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-
 class PublishedManager(models.Manager):
-  def get_queryset(self):
-    return super(PublishedManager, self).get_queryset().filter(status='published')
-
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
-      ('draft', 'Draft'),
-      ('published', 'Published'),
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-                unique_for_date='publish')
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
-                   on_delete=models.CASCADE,
-                   related_name='blog_posts')
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
-                  choices=STATUS_CHOICES,
-                  default='draft')
+                              choices=STATUS_CHOICES,
+                              default='draft')
 
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+    published_date = models.DateTimeField(
+        blank=True, null=True)
 
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager
-    class Meta:
-       ordering = ('-publish',)
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
     def __str__(self):
-      return self.title
+        return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.published_date])
